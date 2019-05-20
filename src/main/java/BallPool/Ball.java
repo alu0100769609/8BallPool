ecd07f3fbf5cd9560bab4daf5b60cb1a73f438bd
Dibad <dibadgithub@gmail.com>
2019-05-20 10:34:43 +0100

    Ball movement

--- src/main/java/BallPool/Ball.java
+++ src/main/java/BallPool/Ball.java
@@ -16,15 +16,20 @@ public class Ball {
   private int posX_;
   private int posY_;
 
-  private float velX_ = 5.0f;
-  private float velY_ = 5.0f;
+  private float vel_ = 7.0f;
+  private float acc_ = 0.08f;
 
-  private static final int MAX_VEL = 5;
+  private static final int MAX_VEL = 10;
+
+  private float xDir_ = 1;
+  private float yDir_ = 1;
+
+  private float angle_ = 50.0f;
 
   private Color ballColor = Color.RED;
 
   private ImageIcon ballIm;
-  
+
   public Ball() {
     posX_ = 50;
     posY_ = 50;
@@ -35,33 +40,36 @@ public class Ball {
 	  posY_ = 50;
 	  ballIm = image;
   }
-  
+
   public Ball(int posX, int posY) {
     posX_ = posX;
     posY_ = posY;
 
-    setVelX((float)rand.nextInt(MAX_VEL));
-    setVelY((float)rand.nextInt(MAX_VEL));
-  }
+    vel_ = (float) rand.nextInt(MAX_VEL) + 1;
 
-  public void setVelX(float velX) {
-    velX_ = velX;
-  }
-
-  public void setVelY(float velY) {
-    velY_ = velY;
+    angle_ = rand.nextInt(360);
   }
 
   public void update(Rectangle boardDimensions) {
+
+    vel_ -= acc_;
+
+    if(vel_ < 0) {
+      vel_ = 0;
+    }
+
+    float velX_ = (float) Math.cos(Math.toRadians(angle_)) * vel_ * xDir_;
+    float velY_ = (float) Math.sin(Math.toRadians(angle_)) * vel_ * yDir_;
+
     posX_ = (int)(posX_ + velX_);
     posY_ = (int)(posY_ + velY_);
 
     if(posX_ < boardDimensions.getX() || posX_ > boardDimensions.getWidth()) {
-      velX_ *= -1;
+      xDir_ *= -1;
     }
 
     if(posY_ < boardDimensions.getY() || posY_ > boardDimensions.getHeight()) {
-      velY_ *= -1;
+      yDir_ *= -1;
     }
   }
 
@@ -76,9 +84,9 @@ public class Ball {
 	    g2d.setColor(ballColor);
 	    g.drawImage(ballIm.getImage(), posX_, posY_, null);
   }*/
-  
+
   public String toString() {
-    return String.format("x: %d. y: %d. vx: %f. vy: %f", posX_, posY_, velX_, velY_);
+    return String.format("x: %d. y: %d. v: %f a: %f", posX_, posY_, vel_, angle_);
   }
 }
 
